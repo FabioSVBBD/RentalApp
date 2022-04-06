4021cde5915b625cd042794d6bff093049475315
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-04-06 19:20:57 +0200

    Created getVehicles proc to return a select of all client details

--- data/scripts/Procedures/VehicleProcedures.sql
+++ data/scripts/Procedures/VehicleProcedures.sql
@@ -47,3 +47,61 @@ AS
 		@Available
 	)
 GO
+
+CREATE PROCEDURE [UpdateVehicle]
+@VIN int,
+@BrandID int,
+@ModelID int,
+@VehicleTypeID int,
+@ColorID int,
+@NumSeats int,
+@DayRate decimal,
+@HourlyRate decimal,
+@YearPurchased int,
+@LastService date,
+@Transmission varchar,
+@Mileage decimal,
+@DepositAmount decimal,
+@Available bit
+AS
+	UPDATE [Vehicle]
+	SET
+		[BrandID] = @BrandID,
+		[ModelID] = @ModelID,
+		[VehicleTypeID] = @VehicleTypeID,
+		[ColorID] = @ColorID,
+		[NumSeats] = @NumSeats,
+		[DayRate] = @DayRate,
+		[HourlyRate] = @HourlyRate,
+		[YearPurchased] = @YearPurchased,
+		[LastService] = @LastService,
+		[Transmission] = @Transmission,
+		[Mileage] = @Mileage,
+		[DepositAmount] = @DepositAmount,
+		[Available] = @Available
+	WHERE @VIN = [VIN]
+GO
+
+CREATE PROCEDURE getVehicles
+AS
+	SELECT 
+		v.VIN, 
+		v.NumSeats, 
+		v.DayRate, 
+		v.HourlyRate, 
+		v.YearPurchased, 
+		v.LastService, 
+		v.Transmission, 
+		v.Mileage, 
+		v.DepositAmount, 
+		v.Available, 
+		b.BrandName,
+		m.ModelName,
+		m.Year,
+		vt.Type
+	FROM Vehicle v 
+	INNER JOIN Color c ON c.ColorID = v.ColorID
+	INNER JOIN Model m ON m.ModelID = v.ModelID
+	INNER JOIN Brand b ON b.BrandID = v.BrandID
+	INNER JOIN VehicleType vt ON vt.VehicleTypeID = v.VehicleTypeID
+GO
