3474aa0839728da42e5bb08671aeb2f0552462bd
Zaeem Asvat <zaeema@bbd.co.za>
2022-04-07 12:08:30 +0200

    added table population script for VehicleType, PaymentMethod, and Review

---
+++ data/scripts/PopulateTablesZaeem.sql
@@ -0,0 +1,28 @@
+USE master;
+GO
+
+USE RentalApp_DB;
+GO
+
+INSERT INTO [dbo].[VehicleType] ([Type])
+VALUES
+    ('Car'),
+    ('Motorcycle'),
+    ('Truck'),
+    ('Van')
+    ('Bus'),
+    ('Trailer')
+
+INSERT INTO [dbo].[PaymentMethod] ([MethodName])
+VALUES
+    ('Cash'),
+    ('Credit Card'),
+    ('Debit Card'),
+    ('Bank Transfer'),
+    ('E-wallet')
+
+INSERT INTO [dbo].[Review] ([Date], [Message], [Rating])
+VALUES
+    (2021-07-14, 'Great service!', 5),
+    (2021-12-09, 'Trash website! Avoid', 1),
+    (2022-03-25, 'Not bad service, but there is room for improvement', 3),
