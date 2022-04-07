89f4017ffc7a5fb9e8febe0e0a479c50e146eae2
Thabo <thaborach@bbd.co.za>
2022-04-07 23:24:55 +0200

    add populate script for client, Status and Employee

---
+++ data/scripts/PopulateTables.sql
@@ -0,0 +1,29 @@
+INSERT INTO [Client] ([Name], Surname, Email, Phone) 
+    VALUES ('Nanine', 'Venners', 'nvenners0@home.pl', '860-318-935'),
+    ('Andre', 'De Souza', 'adesouza1@thetimes.co.uk', '569-660-4441'),
+    ('Carl', 'Ralestone', 'cralestone2@goo.ne.jp', '989-304-5808'),
+    ('Barbaraanne', 'Grunbaum', 'bgrunbaum3@ox.ac.uk', '662-822-4120'),
+    ('Carleen', 'Hacker', 'chacker4@phoca.cz', '801-759-0140'),
+    ('Atlante', 'Boman', 'aboman5@ucla.edu', '259-517-2490'),
+    ('Jessee', 'Starking', 'jstarking6@vistaprint.com', '330-905-6523'),
+    ('Wainwright', 'Gabbitus', 'wgabbitus7@geocities.jp', '732-445-9775'),
+    ('Tobias', 'Estevez', 'testevez8@opera.com', '402-415-1863'),
+    ('Thaddus', 'Renouf', 'trenouf9@google.it', '384-868-5608');
+GO;
+
+INSERT INTO [Status] ([Status]) 
+    VALUES ('Processing'),('Active'),('Cancelled'),('Complete');
+GO;
+
+INSERT INTO [Employee] ([Name], Surname, Phone, DateOfEmployment) 
+    VALUES ('Jack', 'Dielhenn', '967-905-3400', '2021-05-13 00:18:31'),
+    ('Roldan', 'Loveday', '642-116-9328', '2021-08-05 02:31:15'),
+    ('Roana', 'Lanigan', '232-339-6432', '2021-08-14 11:06:04'),
+    ('Buddie', 'Treske', '832-545-0936', '2021-05-12 05:40:55'),
+    ('Isidoro', 'Cavaney', '510-599-9532', '2021-09-16 13:41:44'),
+    ('Frieda', 'Finnigan', '884-725-8732', '2021-09-06 23:20:27'),
+    ('Anderson', 'Hawke', '943-126-8036', '2021-06-21 19:22:27'),
+    ('Tawnya', 'MacShane', '392-988-6191', '2021-03-21 18:42:58'),
+    ('Chancey', 'Aizlewood', '375-433-2127', '2021-12-21 23:26:29'),
+    ('Katinka', 'Rodd', '329-716-8181', '2021-04-13 16:34:56');
+GO;
