95c79781cd80fdb7a1370bfc0b165f80df0d112f
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-04-09 12:17:20 +0200

    Connected to Local SQL Server  DB: RentalApp_DB using [admin, admin] login details

--- src/main/java/com/rental/rentalApp/RentalAppApplication.java
+++ src/main/java/com/rental/rentalApp/RentalAppApplication.java
@@ -1,7 +1,11 @@
 package com.rental.rentalApp;
 
+import com.rental.rentalApp.entities.Client;
+import com.rental.rentalApp.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RentalAppApplication {
@@ -9,5 +13,22 @@ public class RentalAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RentalAppApplication.class, args);
 		System.out.println("Hello World!");
+	}
+
+	@Bean
+	public CommandLineRunner getAllClients(ClientRepository repo) {
+		if (repo.findByName("Jane") .isEmpty()) {
+			repo.save(new Client("Jane", "Johnson", "janejohnson@gmail.com", "0824563433"));
+		}
+
+		return (args) -> {
+			System.out.println("Clients");
+
+			for (Client client : repo.findAll()) {
+				System.out.println(client.toString());
+			}
+
+			System.out.println(String.format("Client with name 'Jane': %s", repo.findByName("Jane").toString()));
+		};
 	}
 }
