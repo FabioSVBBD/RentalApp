8a5faa06d25642e92559c08f5adcd7e9a550edbb
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-04-10 12:11:22 +0200

    Removed unnecessary repo inserts & added config to connect to remote DB

--- src/main/java/com/rental/rentalApp/controller/Controller.java
+++ src/main/java/com/rental/rentalApp/controller/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	
     @GetMapping("rental-app/home")
     public String Home() {
         return "Welcome to Car Rental App!";
