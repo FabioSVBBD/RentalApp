dae3275ce409c18c44f18a4e337f4f9e5eadcf77
Sandhya <sandhyas@bbd.co.za>
2022-04-12 18:49:56 +0200

    feat: created entities

---
+++ src/main/java/com/rental/rentalApp/entities/Color.java
@@ -0,0 +1,31 @@
+package com.rental.rentalApp.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Color {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int ColorID;
+
+    @Column(name = "ColorName")
+    private String colorName;
+
+    protected Color() { }
+
+    public Color(String colorName) {
+      this.setColorName(colorName);
+  }
+
+  @Override
+  public String toString() {
+      return String.format("Color => ID: "+ ColorID +" ColorName: " + colorName);
+  }
+
+  public String getColorName() { return this.colorName; }
+
+  public void setColorName(String colorName) {
+      this.colorName = colorName;
+  }
+}
