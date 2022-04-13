f6fcb45a39837235f72444a09598c51a66968a1e
Lusanda Mthiba <61183124+luslindo@users.noreply.github.com>
2022-04-13 12:10:06 +0200

    Update src/main/java/com/rental/rentalApp/RentalAppApplication.java
    Co-authored-by: Fabio Sousa Vieira <95920140+FabioSVBBD@users.noreply.github.com>

--- src/main/java/com/rental/rentalApp/RentalAppApplication.java
+++ src/main/java/com/rental/rentalApp/RentalAppApplication.java
@@ -1,9 +1,7 @@
 package com.rental.rentalApp;
 
-import com.rental.rentalApp.entities.Client;
-import com.rental.rentalApp.entities.Status;
-import com.rental.rentalApp.repositories.ClientRepository;
-import com.rental.rentalApp.repositories.StatusRepository;
+import com.rental.rentalApp.entities.*;
+import com.rental.rentalApp.repositories.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
