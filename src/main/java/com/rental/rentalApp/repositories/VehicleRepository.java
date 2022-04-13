afe19d0c2fb50cb346c15dbe09508d33991e9381
zaeemasvatbbd <99711395+zaeemasvatbbd@users.noreply.github.com>
2022-04-13 16:16:43 +0200

    Update src/main/java/com/rental/rentalApp/repositories/VehicleRepository.java
    Co-authored-by: Fabio Sousa Vieira <95920140+FabioSVBBD@users.noreply.github.com>

--- src/main/java/com/rental/rentalApp/repositories/VehicleRepository.java
+++ src/main/java/com/rental/rentalApp/repositories/VehicleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface VehicleRepository extends CrudRepository<Vehicle, Integer> {
-//    List<Vehicle> findAllVehicles();
+    List<Vehicle> findByVIN();
 }
