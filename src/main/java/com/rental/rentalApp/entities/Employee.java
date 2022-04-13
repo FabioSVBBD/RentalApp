4501947388ecc8c1953798fe6a1004cb0d41b2dd
Carmen <carmen@bbd.co.za>
2022-04-13 19:45:34 +0200

    Added Employee class

--- src/main/java/com/rental/rentalApp/entities/Employee.java
+++ src/main/java/com/rental/rentalApp/entities/Employee.java
@@ -5,57 +5,30 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-public class Employee {
+public class Employee extends Person{
+    
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "EmployeeID")
-    private Long id;
-
-    @Column(name = "Name")
-    private String name;
-
-    @Column(name = "Surname")
-    private String surname;
-
-    @Column(name = "PhoneNo")
-    private String phoneNo;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected Integer employeeID;
 
     @Column(name = "dateOfEmployment")
     private LocalDateTime dateOfEmployment;
 
     protected Employee(){}
 
-    Employee(String name, String surname, String phoneNo, LocalDateTime dateOfEmployment){
-        this.name = name;
-        this.surname = surname;
-        this.phoneNo = phoneNo;
+    Employee(String name, String surname, String number, LocalDateTime dateOfEmployment){
+    	super(name, surname, number);
         this.dateOfEmployment = dateOfEmployment;
     }
 
     @Override
     public String toString() {
-        return "Employee [ id = "+id+", name = "+name+", surname = "+surname+", phoneNo="+phoneNo+", dateOfEmployment = "+dateOfEmployment+" ]";
+        return "Employee [ id = "+ employeeID +", name = "+name+", surname = "+surname+", phone="+ number +", dateOfEmployment = "+dateOfEmployment+" ]";
     }
 
-    // Accessors
-    public String getName() { return this.name; }
-    public String getSurname() { return this.surname; }
-    public String getPhoneNo() { return this.phoneNo; }
+    
     public LocalDateTime getDateOfEmployment() { return this.dateOfEmployment; }
-
-    // Mutators
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setPhoneNo(String phoneNo) {
-        this.phoneNo = phoneNo;
-    }
 
     public void setDateOfEmployment(LocalDateTime dateOfEmployment) {
         this.dateOfEmployment = dateOfEmployment;
