92c50efd584e365c9044170ad9d44b66e8d0d4c8
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-04-13 18:49:25 +0200

    Created Person class and edited Client

--- src/main/java/com/rental/rentalApp/entities/Client.java
+++ src/main/java/com/rental/rentalApp/entities/Client.java
@@ -3,57 +3,31 @@ package com.rental.rentalApp.entities;
 import javax.persistence.*;
 
 @Entity
-public class Client {
+public class Client extends Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int ClientID;
-
-    @Column(name = "Name")
-    private String name;
-
-    @Column(name = "Surname")
-    private String surname;
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		protected Integer clientID;
 
     @Column(name = "Email")
     private String email;
 
-    @Column(name = "Phone")
-    private String number;
-
     protected Client() { }
 
     public Client(String name, String surname, String email, String number) {
-        this.setName(name);
-        this.setSurname(surname);
-        this.setEmail(email);
-        this.setName(number);
+        super(name, surname, number);
+				this.setEmail(email);
     }
 
     @Override
     public String toString() {
         return String.format("Client [ id = %d, name = %s, surname= %s, email= %s, phone = %s ]",
-                ClientID, name, surname, email, number);
+                clientID, name, surname, email, number);
     }
 
-    public String getName() { return this.name; }
-    public String getSurname() { return this.surname; }
     public String getEmail() { return this.email; }
-    public String getNumber() { return this.number; }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
     }
 }
