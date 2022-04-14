9a49c09c0d0183de033c4bdaccda3acd0ebeaa73
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-04-14 19:51:50 +0200

    unformatted employee

--- src/main/java/com/rental/rentalApp/entities/Employee.java
+++ src/main/java/com/rental/rentalApp/entities/Employee.java
@@ -1,14 +1,16 @@
 package com.rental.rentalApp.entities;
 
 import javax.persistence.*;
+
 import java.time.LocalDateTime;
 
 @Entity
 public class Employee extends Person{
     
-		@Id
-		@GeneratedValue(strategy = GenerationType.AUTO)
-		protected Integer employeeID;
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected Integer employeeID;
 
     @Column(name = "dateOfEmployment")
     private LocalDateTime dateOfEmployment;
@@ -16,18 +18,19 @@ public class Employee extends Person{
     protected Employee(){}
 
     Employee(String name, String surname, String number, LocalDateTime dateOfEmployment){
-				super(name, surname, number);
-				this.dateOfEmployment = dateOfEmployment;
+    	super(name, surname, number);
+        this.dateOfEmployment = dateOfEmployment;
     }
 
     @Override
     public String toString() {
         return "Employee [ id = "+ employeeID +", name = "+name+", surname = "+surname+", phone="+ number +", dateOfEmployment = "+dateOfEmployment+" ]";
     }
+
     
     public LocalDateTime getDateOfEmployment() { return this.dateOfEmployment; }
 
     public void setDateOfEmployment(LocalDateTime dateOfEmployment) {
-				this.dateOfEmployment = dateOfEmployment;
+        this.dateOfEmployment = dateOfEmployment;
     }
 }
