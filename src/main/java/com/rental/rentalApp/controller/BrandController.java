c9582a9d205c4812b68c6a427971eb1d6909305b
Thabo <thaborach@bbd.co.za>
2022-04-18 17:07:37 +0200

    feature - add getAllBrands

---
+++ src/main/java/com/rental/rentalApp/controller/BrandController.java
@@ -0,0 +1,27 @@
+package com.rental.rentalApp.controller;
+
+import java.util.*;
+
+import com.rental.rentalApp.entities.Brand;
+import com.rental.rentalApp.repositories.BrandRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("rental-app/api/brands")
+public class BrandController {
+
+    private final BrandRepository brandRepository;
+
+	private BrandController(BrandRepository repository) {
+		this.brandRepository = repository;
+	}
+
+    // Get all brands
+	@GetMapping("")
+    ResponseEntity<List<Brand>> getAllBrands() {
+		return ResponseEntity.ok(brandRepository.findAll());
+    }
+    
+}
