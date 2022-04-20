3f67e3e7822b84b52d58f2c0bcc95164eca83d55
zaeemasvatbbd <99711395+zaeemasvatbbd@users.noreply.github.com>
2022-04-20 13:14:58 +0200

    Update RentalAppApplication.java

--- src/main/java/com/rental/rentalApp/RentalAppApplication.java
+++ src/main/java/com/rental/rentalApp/RentalAppApplication.java
@@ -2,7 +2,6 @@ package com.rental.rentalApp;
 
 import com.rental.rentalApp.entities.*;
 import com.rental.rentalApp.repositories.*;
-import com.rental.rentalApp.services.*;
 import org.springframework.boot.CommandLineRunner;
 
 import org.springframework.boot.SpringApplication;
@@ -17,15 +16,4 @@ public class RentalAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RentalAppApplication.class, args);
 	}
-//
-//	@Bean
-//	public CommandLineRunner getTotalCost(RentalService rentalService)
-//	{
-//		return (args) -> {
-//			System.out.println("Total Cost");
-//			Duration duration = rentalService.getDuration(1);
-//			System.out.println(duration);
-//		};
-//	}
-
 }
