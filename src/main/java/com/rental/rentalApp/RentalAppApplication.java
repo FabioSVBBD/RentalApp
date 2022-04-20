6d101db0248792937b7272b9247e72ed8ef65c3c
Zaeem Asvat <zaeema@bbd.co.za>
2022-04-20 13:19:38 +0200

    removed commented code

--- src/main/java/com/rental/rentalApp/RentalAppApplication.java
+++ src/main/java/com/rental/rentalApp/RentalAppApplication.java
@@ -2,14 +2,10 @@ package com.rental.rentalApp;
 
 import com.rental.rentalApp.entities.*;
 import com.rental.rentalApp.repositories.*;
-import com.rental.rentalApp.services.*;
 import org.springframework.boot.CommandLineRunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.math.BigDecimal;
-import java.time.Duration;
 
 @SpringBootApplication
 public class RentalAppApplication {
@@ -17,15 +13,5 @@ public class RentalAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RentalAppApplication.class, args);
 	}
-//
-//	@Bean
-//	public CommandLineRunner getTotalCost(RentalService rentalService)
-//	{
-//		return (args) -> {
-//			System.out.println("Total Cost");
-//			Duration duration = rentalService.getDuration(1);
-//			System.out.println(duration);
-//		};
-//	}
 
 }
