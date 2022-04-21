16238f93dca66e401e26ea26eebf486c875ac16a
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-04-21 09:41:05 +0200

    fixed employee post

--- src/main/java/com/rental/rentalApp/repositories/EmployeeRepository.java
+++ src/main/java/com/rental/rentalApp/repositories/EmployeeRepository.java
@@ -10,10 +10,15 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 	List<Employee> findByName(String name);
+
 	List<Employee> findBySurname(String surname);
+
 	List<Employee> findByNumber(String number);
-	List<Employee> findByDateOfEmployment(LocalDate dateOfEmployment); 
+
+	List<Employee> findByDateOfEmployment(LocalDate dateOfEmployment);
+
 	List<Employee> findByDateOfEmploymentGreaterThanEqual(LocalDate dateOfEmployment);
+
 	List<Employee> findByNameOrSurname(String name, String surname);
-	
+
 }
